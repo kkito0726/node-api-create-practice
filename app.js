e14807f5be57773208b13fab60467cdcc09a1157
kkito0726 <k.kito0726@gmail.com>
2023-01-04 23:20:46 +0900

    mysqlに接続してデータベース連携できるようにした

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require("express");
+const mysql = require("mysql2");
 const app = express();
 const port = 4000;
 const bookLogs = [];
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Example_db01",
+  database: "booklog",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("success");
+});
+
+const getDataBase = () => {
+  connection.query("SELECT * FROM booklog", (err, response) => {
+    if (err) throw err;
+    console.log(response);
+  });
+};
 
 app.use(express.json());
 app.listen(port, () => {
@@ -12,16 +35,25 @@ app.get("/", (req, res) => {
   res.send("sever");
 });
 
+// データベースの情報を取得
 app.get("/booklog", (req, res) => {
-  res.json(bookLogs);
+  // res.json(bookLogs);
+  getDataBase();
+  res.send("Get DB Table");
 });
 
+// レコードを追加
 app.post("/booklog", (req, res) => {
   const bookLog = {
-    id: bookLogs.length + 1,
     title: req.body.title,
     comment: req.body.comment,
   };
+  // const q = "insert to booklog values SET ?";
+  const q = "insert into booklog(title, comment) values(?,?)";
+  connection.query(q, [bookLog.title, bookLog.comment], (err, result) => {
+    if (err) throw err;
+    getDataBase();
+  });
   // 例外処理
   if (!(bookLog.title && bookLog.comment)) {
     return res.json({
@@ -31,16 +63,18 @@ app.post("/booklog", (req, res) => {
   }
 
   //   レスポンス
-  bookLogs.push(bookLog);
-  res.json(bookLog);
+  // bookLogs.push(bookLog);
+  // res.json(bookLog);
+  res.send("Add record to Table");
 });
 
+// 既存のレコードを更新
 app.put("/booklog/:id", (req, res) => {
-  const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
-  console.log(req.params);
-  if (!bookLog) {
-    res.status(404).send("Not Found");
-  }
+  // const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
+  // console.log(req.params);
+  // if (!bookLog) {
+  //   res.status(404).send("Not Found");
+  // }
   // 例外処理
   if (!(req.body.title && req.body.comment)) {
     return res.json({
@@ -49,23 +83,40 @@ app.put("/booklog/:id", (req, res) => {
     });
   }
 
-  bookLog.title = req.body.title;
-  bookLog.comment = req.body.comment;
-  res.send(bookLog);
+  const q = `UPDATE booklog SET title=?, comment=? WHERE id=${String(
+    req.params.id
+  )}`;
+  connection.query(q, [req.body.title, req.body.comment], (err) => {
+    if (err) throw err;
+    getDataBase();
+  });
+  // bookLog.title = req.body.title;
+  // bookLog.comment = req.body.comment;
+  res.send(`updated id = ${req.body.comment}`);
 });
 
+//レコードの削除
 app.delete("/booklog/:id", (req, res) => {
-  const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
-  if (!bookLog) {
-    res.status(404).send("Not Found");
-  }
+  // const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
+  // if (!bookLog) {
+  //   res.status(404).send("Not Found");
+  // }
 
-  const index = bookLogs.indexOf(bookLog);
-  bookLogs.splice(index, 1);
+  // const index = bookLogs.indexOf(bookLog);
+  const str_idx = String(req.params.id);
+  // bookLogs.splice(index, 1);
+
+  const q = `delete from booklog where id = ${str_idx}`;
+  console.log(q);
+  connection.query(q, (err) => {
+    if (err) throw err;
+    getDataBase();
+  });
 
   // idに抜け落ちがないようにつけ直す
-  bookLogs.map((bookLog, idx) => {
-    bookLog.id = idx + 1;
-  });
-  res.json(bookLog);
+  // bookLogs.map((bookLog, idx) => {
+  //   bookLog.id = idx + 1;
+  // });
+  // res.json(bookLog);
+  res.send(`Deleted id = ${str_idx}`);
 });
