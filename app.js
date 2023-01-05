373a8c74b6836ead5d189ed62a9ec99fc4823647
kkito0726 <k.kito0726@gmail.com>
2023-01-05 15:03:41 +0900

    ♻️ 無駄な処理を省き、読みやすい順番に処理を並び替えた

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql2");
 const app = express();
 const port = 4000;
-const bookLogs = [];
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -20,20 +19,7 @@ connection.connect((err) => {
   console.log("success");
 });
 
-const getDataBase = () => {
-  connection.query(
-    "SELECT * FROM booklog WHERE deleted_at IS NULL",
-    (err, response) => {
-      if (err) throw err;
-      console.log(response);
-    }
-  );
-};
-
 app.use(express.json());
-app.listen(port, () => {
-  console.log("sever started");
-});
 
 app.get("/", (req, res) => {
   res.send("sever");
@@ -41,9 +27,13 @@ app.get("/", (req, res) => {
 
 // データベースの情報を取得
 app.get("/booklog", (req, res) => {
-  // res.json(bookLogs);
-  getDataBase();
-  res.send("Get DB Table");
+  connection.query(
+    "SELECT * FROM booklog WHERE deleted_at IS NULL",
+    (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    }
+  );
 });
 
 // レコードを追加
@@ -52,13 +42,6 @@ app.post("/booklog", (req, res) => {
     title: req.body.title,
     comment: req.body.comment,
   };
-
-  // const q = "insert into booklog SET ?"; この書き方だとSQLインジェクションができてしまう
-  const q = "INSERT INTO booklog (title, comment) VALUE (?, ?)";
-  connection.query(q, [bookLog.title, bookLog.comment], (err) => {
-    if (err) throw err;
-    getDataBase();
-  });
 
   // 例外処理
   if (!(bookLog.title && bookLog.comment)) {
@@ -73,16 +56,13 @@ app.post("/booklog", (req, res) => {
   connection.query(q, [bookLog.title, bookLog.comment], (err) => {
     if (err) throw err;
   });
+
   res.send("Add record to Table");
 });
 
 // 既存のレコードを更新
 app.put("/booklog/:id", (req, res) => {
-  // const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
-  // console.log(req.params);
-  // if (!bookLog) {
-  //   res.status(404).send("Not Found");
-  // }
+  const id = req.params.id;
   // 例外処理
   if (!(req.body.title && req.body.comment)) {
     return res.json({
@@ -95,38 +75,26 @@ app.put("/booklog/:id", (req, res) => {
     comment: req.body.comment,
   };
 
-  const q = `UPDATE booklog SET ? WHERE id=?`;
-  connection.query(q, [bookLog, String(req.params.id)], (err) => {
+  const q = `UPDATE booklog SET title=?, comment=? WHERE id=? AND deleted_at IS NULL`;
+  connection.query(q, [bookLog.title, bookLog.comment, id], (err) => {
     if (err) throw err;
-    getDataBase();
   });
-  // bookLog.title = req.body.title;
-  // bookLog.comment = req.body.comment;
-  res.send(`updated id = ${req.body.comment}`);
+
+  res.send(`updated id = ${id}`);
 });
 
 //レコードの削除
 app.delete("/booklog/:id", (req, res) => {
-  // const bookLog = bookLogs.find((c) => c.id === parseInt(req.params.id));
-  // if (!bookLog) {
-  //   res.status(404).send("Not Found");
-  // }
-
-  // const index = bookLogs.indexOf(bookLog);
   const str_idx = String(req.params.id);
-  // bookLogs.splice(index, 1);
 
   const q = "UPDATE booklog SET deleted_at=? WHERE id=?";
-  console.log(q);
   connection.query(q, [new Date(), str_idx], (err) => {
     if (err) throw err;
-    getDataBase();
   });
 
-  // idに抜け落ちがないようにつけ直す
-  // bookLogs.map((bookLog, idx) => {
-  //   bookLog.id = idx + 1;
-  // });
-  // res.json(bookLog);
   res.send(`Deleted id = ${str_idx}`);
+});
+
+app.listen(port, () => {
+  console.log("sever started");
 });
