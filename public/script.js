bd776364b46efe4cdc4e213c61d2ab6019ab758b
kkito0726 <k.kito0726@gmail.com>
2023-01-11 17:22:30 +0900

    ✨ POSTとDELETEをクライアントサイドから行えるようにした

--- public/script.js
+++ public/script.js
@@ -2,6 +2,7 @@ const url = "http://localhost:4000";
 console.log(url);
 const titleRef = document.querySelector("#title");
 const commentRef = document.querySelector("#comment");
+const deleteIdRef = document.querySelector("#deleteId");
 
 const getDatabase = async () => {
   const res = await fetch(`${url}/booklog`);
@@ -15,17 +16,20 @@ const getDatabase = async () => {
   });
 };
 
-const postDatabese = async () => {
+const postDatabase = async () => {
   const title = titleRef.value;
   const comment = commentRef.value;
 
-  const formData = new FormData();
-  formData.append("title", title);
-  formData.append("comment", comment);
+  // const formData = new FormData();
+  // formData.append("title", title);
+  // formData.append("comment", comment);
 
   const res = await fetch(`${url}/booklog`, {
     method: "POST",
-    body: JSON.stringify({ title, comment }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title: title, comment: comment }),
   });
   // const res = await fetch(`${url}/booklog`, {
   //   method: "POST",
@@ -37,6 +41,15 @@ const postDatabese = async () => {
   //   body: JSON.stringify({ title: title, comment: comment }),
   // });
   console.log(res.status);
+  console.log(JSON.stringify({ title, comment }));
+};
+
+const deleteDatabase = async (id) => {
+  const res = await fetch(`${url}/booklog/${id}`, {
+    method: "DELETE",
+  });
+
+  console.log(res.status);
 };
 
 const getButton = document.querySelector("#get");
@@ -45,7 +58,15 @@ getButton.addEventListener("click", () => {
 });
 
 const postButton = document.querySelector("#post");
-postButton.addEventListener("click", async () => {
-  await postDatabese();
-  await getDatabase();
+postButton.addEventListener("click", () => {
+  postDatabase();
+  getDatabase();
+});
+
+const deleteButton = document.querySelector("#deleteButton");
+deleteButton.addEventListener("click", () => {
+  const deleteId = deleteIdRef.value;
+  console.log(deleteId);
+  deleteDatabase(deleteId);
+  getDatabase();
 });
